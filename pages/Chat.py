dfb58ce86b08055cf913c3f2ab99668258db33d8
Sufokaio <taha.el.hihi@hotmail.com>
2024-12-01 21:08:38 +0100

    please

--- pages/Chat.py
+++ pages/Chat.py
@@ -51,6 +51,7 @@ contrats_index = FAISS.load_local("contrats" + "_index", embeddings, allow_dange
 
 
 
+
 class Chatbot:
 
     def translate_text(self, text, target_lang):    
@@ -82,6 +83,25 @@ class Chatbot:
     
     openai = OpenAI()
 
+    def generate_query_variations(self, query, model="gpt-4"):
+        system_prompt = (
+            "Tu es un assistant qui génère plusieurs variantes de requêtes pour améliorer les résultats de recherche. "
+            "Garde le sens original de la question, mais reformule de manière claire et précise. "
+            "Renvoie les variantes sous forme de liste séparée par des sauts de ligne."
+        )
+        user_prompt = f"Voici la question de l'utilisateur : {query}\nGénère 5 variantes."
+
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+        response = self.openai.chat.completions.create(
+            model = "gpt-4o",
+            messages = messages
+        )
+        variations = response.choices[0].message.content.strip().split("\n")
+        return variations
+
     def __init__(self):
         print("Starting the chatbot")
 
@@ -93,7 +113,18 @@ class Chatbot:
         else:
             index = contrats_index
 
-        results = index.similarity_search(query, k=3)            
+        query_variations = self.generate_query_variations(query)
+
+        all_results = []
+
+        for variation in query_variations:
+            results = index.similarity_search(variation, k=3)  
+            all_results.extend(results)
+
+        unique_results = {result.page_content for result in all_results}
+        sources_list = list(unique_results)[:4]
+
+
         sources_list = [result.page_content for result in results]
         user_prompt = f"Voici la question de l'utilisateur sur le droit du travail: {query}\nvoici les sources que tu dois utiliser pour répondre a cette question: {sources_list}"
 
@@ -112,7 +143,7 @@ class Chatbot:
 
 
     def get_answer(self, query: str, speciality: str) -> str:
-        system_prompt = "tu dois me dire si la phrase donnée est en marocain ou en français. ne répond qu'avec 'fraçais' ou 'arabe' et aucun autre mot"
+        system_prompt = "tu dois me dire si la phrase donnée est en arabe ou en français. ne répond qu'avec 'français' ou 'arabe' et aucun autre mot"
         user_prompt = f"Voici la phrase que tu dois classifier: {query}"
         messages = [
             {"role": "system", "content": system_prompt},
